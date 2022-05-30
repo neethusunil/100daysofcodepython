4cef245e937037feedd29038f5eef42bf78fbedb
Neethu Sunil <neethu@toasoft.onmicrosoft.com>
2022-05-30 16:18:34 +0900

    first commit

---
+++ d1_band name.py
@@ -0,0 +1,7 @@
+#day 1 /　100 days of code
+
+print("Welcome to the Band Name Generator.")
+city_name = input("What's name of the city you grew up in?")
+pet_name = input("What's your pet's name?")
+band_name = city_name + " " + pet_name
+print("Your band name could be " +band_name)
